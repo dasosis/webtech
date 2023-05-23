04ceffc7b81743530e900c1bbd15783f8893fc86
dasosis <soumyasnigdhadas@gmail.com>
2023-05-23 15:11:56 +0530

    complete

--- a14/script.js
+++ a14/script.js
@@ -1,19 +1,16 @@
 document.getElementById('commentForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Get the comment text from the textarea
+    event.preventDefault(); 
+
     var commentText = document.getElementById('commentText').value;
   
     if (commentText.trim() !== '') {
-      // Create a new list item for the comment
+
       var commentItem = document.createElement('li');
       commentItem.textContent = commentText;
-  
-      // Add the new comment to the comment list
+
       var commentList = document.getElementById('commentList');
       commentList.appendChild(commentItem);
-  
-      // Clear the textarea after adding the comment
+
       document.getElementById('commentText').value = '';
     }
   });
